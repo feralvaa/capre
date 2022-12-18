86659988d9adfd0cca6c39d320438252de91eb73
Fer Alva <feralvaa@hotmail.com>
2022-12-18 17:28:17 -0500

    third

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# secreatkey
+# app.config['SECRET_KEY'] =  ''
+#dictionary
+# posts = [ 
+#     {
+#         'author' : 'Corey Schafer',
+#         'title' : 'Blog Post 1'
+#     },
+#     { 
+#         'author' : 'Queen',
+#         'title' : 'Cest LaVie'
+#     }
+# ]
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+     return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,27 @@
+from flasl-wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Datarequired, Lenght, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    #check validation / Data required -> 
+    username = StringField ('Username',
+                             validators= [DataRequired(), Length(min=2, max=20 )])
+
+    email = StringField ('Email',
+                            validators= [DataRequired(),Email()])
+
+    #Passwords
+    password = PasswordField('Password', validators = [DataRequired()])
+    confirm_password = PasswordField ('Confirm Password', 
+                                        validators=[DataRequired(),EqualTo('password')])
+    submit= SubmitField('Sign Up')
+
+class Login(FlaskForm):
+   
+    email = StringField ('Email',
+                            validators= [DataRequired(),Email()])
+
+    password = PasswordField('Password', validators = [DataRequired()])
+    remeber = BooleanField('Remember Me')
+    submit= SubmitField('Login')
+    
