b89a2f6740e6a71d0df228980f84b67ce8d30fcb
Fer Alva <feralvaa@hotmail.com>
2022-12-18 19:46:34 -0500

    antes de javi

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
 from flask import Flask, render_template
+# from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
-
-# secreatkey
-# app.config['SECRET_KEY'] =  ''
-#dictionary
-# posts = [ 
-#     {
-#         'author' : 'Corey Schafer',
-#         'title' : 'Blog Post 1'
-#     },
-#     { 
-#         'author' : 'Queen',
-#         'title' : 'Cest LaVie'
-#     }
-# ]
-
+# secret key
+app.config['SECRET_KEY'] =  '424587ba6b2905c5075ee379cabf60f5767936995e78a754b9786f517c994d76'
 
 @app.route("/")
 @app.route("/home")
@@ -24,6 +13,17 @@ def home():
 @app.route("/about")
 def about():
      return render_template('about.html')
+
+
+# @app.route("/register")
+# def register():
+#      form = RegistrationForm()
+#      return redender_template ('register.html', title= 'Register', form=form)
+
+# @app.route("/login")
+# def login():
+#      form = LoginForm()
+#      return redender_template ('login.html', title= 'Login', form=form)
 
 
 if __name__ == '__main__':
